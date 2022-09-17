adc1a9e3723714d467cb3a3dea52d00918ef5051
Javier Porras <jporras06@gmail.com>
2022-09-16 18:13:40 -0600

    Exported some stuff

--- oura_nbdev_dashboard/_modidx.py
+++ oura_nbdev_dashboard/_modidx.py
@@ -5,4 +5,8 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://jotaporras.github.io',
                 'git_url': 'https://github.com/jotaporras/oura-nbdev-dashboard',
                 'lib_path': 'oura_nbdev_dashboard'},
-  'syms': {'oura_nbdev_dashboard.core': {'oura_nbdev_dashboard.core.foo': ('core.html#foo', 'oura_nbdev_dashboard/core.py')}}}
+  'syms': { 'oura_nbdev_dashboard.core': {'oura_nbdev_dashboard.core.foo': ('core.html#foo', 'oura_nbdev_dashboard/core.py')},
+            'oura_nbdev_dashboard.total_sleep': { 'oura_nbdev_dashboard.total_sleep.foo': ( 'first_nbdev.html#foo',
+                                                                                            'oura_nbdev_dashboard/total_sleep.py'),
+                                                  'oura_nbdev_dashboard.total_sleep.say_hello': ( 'first_nbdev.html#say_hello',
+                                                                                                  'oura_nbdev_dashboard/total_sleep.py')}}}
